d57af03f3223ba5b342118ee83abbdebe15ebb03
DanilDerypaska <82759878+DanilDerypaska@users.noreply.github.com>
2021-04-18 18:31:36 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        // Завдання 1
+        public static double Sk(double R, double r)
+        {
+            double result = Math.PI* R *R - Math.PI * r * r;
+            return result;
+        }
+        // Завдання 2
+        public static double S(double n, double x)
+        {
+            double result = x-(x*x*x)/3+ Math.Pow(x, 5)/5+ Math.Pow(-1, n)*((Math.Pow(x, 2*n+1)) / (2*n + 1));
+            return result;
+        }
+        // Завдання 3
+        public static double st(double x, double n)
+        {
+            double st = x - (x * x * x) / 3 + Math.Pow(x, 5) / 5 - Math.Pow(x, 7) / 7 + Math.Pow(-1, n) * ((Math.Pow(x, 2 * n + 1)) / (2 * n + 1));
+            double arcctg = Math.PI / 2 - Math.Atan(x);
+            double t = st / arcctg;
+            return t;
+        }        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Завдання 1\nSk="+ Class1.Sk(5, 3));
+            Console.WriteLine("Завдання 2\nS=" + Class1.S(2, 3));
+            Console.WriteLine("Завдання 3\nt="+ Class1.st(2,8));
+        }
+    }
+}
